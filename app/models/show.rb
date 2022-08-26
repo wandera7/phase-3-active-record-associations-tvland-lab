30c7d62902e254c5f0e6a049c6fd8d6131c0e95e
wandera7 <julius.wandera@student.moringaschool.com>
2022-08-26 20:29:22 +0300

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors , through: :characters
+
+  def full_name
+    "#{self.actors.first_name} #{self.actors.last_name}"
+  end
+  def actors_list
+    self.actors.map {|act| "#{act.first_name} #{act.last_name}"}
+  end
+
 end
